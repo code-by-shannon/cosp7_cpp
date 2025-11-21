6efca6531605ab99f6cfa7a1bbf932bd6dab6a8a
Shannon <sestumpf@gmail.com>
2025-11-20 17:23:12 -0800

    Initial commit for cosp7_c++

---
+++ ass20.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int speed_limit_check(){
+    int speed;
+    cout << "Please enter the speed at which vehicle was traveling: ";
+    cin >> speed;
+
+    while(speed < 20 || speed > 70){
+        cout << "Please enter a speed between 20 and 70";
+        cin >> speed;
+    }
+
+    return speed;
+}
+
+int main()
+{
+    int checked_speed = speed_limit_check();
+    cout << "This was the speed you entered and it was in range: " << checked_speed;
+    return 0;
+}
