5f708f8bf9c8fc8248fe4b5bda4ef8093cb4d405
Shannon <sestumpf@gmail.com>
2025-11-20 17:39:54 -0800

    speed limit creds updated

--- ass20.cpp
+++ ass20.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int speed_limit_check(){
     int speed;
-    cout << "Please enter the speed at which vehicle was traveling: ";
+    cout << "Please enter the speed limit in question: ";
     cin >> speed;
 
     while(speed < 20 || speed > 70){
-        cout << "Please enter a speed between 20 and 70";
+        cout << "Please enter a speed limit between 20 and 70" << endl;
         cin >> speed;
     }
 
@@ -19,8 +19,7 @@ int speed_limit_check(){
 int main()
 {
     int checked_speed = speed_limit_check();
-    cout << "This was the speed you entered and it was in range: " << checked_speed;
+    cout << "This is the speed limit you entered: " << checked_speed << endl;
     return 0;
 }
 
-nothing burger
