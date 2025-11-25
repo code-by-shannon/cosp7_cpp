29eacdca4b77884f40199817f84d38302070f3e3
Shannon <sestumpf@gmail.com>
2025-11-25 12:17:12 -0800

    Add ass21.cpp

---
+++ ass21.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+//  function that returns something
+
+// function that returns something
+
+int main()
+{
+    // set up consts that = funcion call and return something
+    return 0;
+}
+
