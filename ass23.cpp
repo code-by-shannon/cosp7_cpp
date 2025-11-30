bc0f74d8baebc7ce82f2839a364141160ed3da8b
Shannon <sestumpf@gmail.com>
2025-11-29 19:08:43 -0800

    ass23 complete

--- ass23.cpp
+++ ass23.cpp
@@ -2,61 +2,36 @@
 #include <iomanip>
 using namespace std;
 
-double users_random_numbers[20];
+int valid_account_numbers[18] = {5658845, 4520125, 7895122, 8777541, 8451277,
+1302850, 8080152, 4562555, 5552012, 5050552, 7825877, 1250255, 1005231, 6545231,
+3852085, 7576651, 7881200, 4581002};
 
-string enter_number_prompt[20] = {"Number 1: ", 
-                              "Number 2:", 
-                              "Number 3:", 
-                              "Number 4:", 
-                              "Number 5:", 
-                              "Number 6: ", 
-                              "Number 7:", 
-                              "Number 8:", 
-                              "Number 9:", 
-                              "Number 10:", 
-                              "Number 11: ", 
-                              "Number 12:", 
-                              "Number 13:", 
-                              "Number 14:", 
-                              "Number 15:", 
-                              "Number 16: ", 
-                              "Number 17:", 
-                              "Number 18:", 
-                              "Number 19:", 
-                              "Number 20:"
-                            };
-                           
 
-double total = 0;
 
 
 int main()
-{
-    for (int i = 0; i < 20; i++){
-      cout << "Please enter a random number: " << enter_number_prompt[i] << endl;
-      cin >> users_random_numbers[i];
-      total += users_random_numbers[i];
+{   
+    int user_number;
+    bool found = false;
+
+
+    
+    cout << "Please enter your account number: " << endl;
+    cin >> user_number;
+
+    for (int i = 0; i < 18; i++){
+        if(valid_account_numbers[i] == user_number){
+            found = true;
+            cout << "Your account number is valid";
+            break;
+        } 
     }
 
-    double lowest = users_random_numbers[0];
-    for (int i = 1; i < 20; i++){
-        if (users_random_numbers[i] < lowest){
-            lowest = users_random_numbers[i];
-        } ;
+    if (!found){
+        cout << "Your account number is not valid";
     }
+    
 
-     double highest = users_random_numbers[0];
-    for (int i = 1; i < 20; i++){
-        if (users_random_numbers[i] > highest){
-            highest = users_random_numbers[i];
-        } ;
-    }
-
-    cout << fixed << setprecision(2);
-    cout << "Total value of added numbers: " << total << endl;
-    cout << "Average of added numbers: " << total/20 << endl;
-    cout << "Lowest value the user entered is: " << lowest << endl;
-    cout << "Highest value the user entered is: " << highest << endl;
 
     return 0;
 }
