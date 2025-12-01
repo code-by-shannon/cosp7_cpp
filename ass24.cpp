707b3605c6fc56a49f5921956acffc6bc1d3cd1d
Shannon <sestumpf@gmail.com>
2025-12-01 12:54:53 -0800

    finished ass24

--- ass24.cpp
+++ ass24.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 
@@ -8,8 +9,16 @@ using namespace std;
 
 int main()
 {   
+    ifstream infile("numbers.dat");
 
-    
+    int num;
+    int total = 0;
+
+    while (infile >> num) {
+        total += num;
+    }
+
+    cout << "Total of all values in numbers file: " << total << endl;
 
 
     return 0;
